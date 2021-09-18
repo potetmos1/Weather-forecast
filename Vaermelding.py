6ca18d0a11a38853aeea4ed7ebfd1c3d7aca2791
potetmos1 <petterssateroy@gmail.com>
2021-09-18 03:25:54 +0200

    daily check

--- Vaermelding.py
+++ Vaermelding.py
@@ -1,20 +1,25 @@
 from yr.libyr import Yr
-import json
-import datetime
+import json, datetime, schedule, time
 
 weather = Yr(location_name='Norway/Oslo/Oslo/Oslo', forecast_link='forecast_hour_by_hour')
 
-klokkeslett = datetime.datetime(1984,1,1,00,0,0)
+def sjekkVaer():
+    klokkeslett = datetime.datetime(1984,1,1,00,0,0)
+    for forecast in weather.forecast(str):
+        data = json.loads(forecast)
+        temp = data['temperature']
+        rain = data['precipitation']
+        regn = rain['@value']
+        temperatur = temp['@value']
+        if(klokkeslett.minute == 0):
+            print(f"kl {klokkeslett.hour}:00 - {temperatur} - {regn}mm")
+        else:
+            print(f"kl {klokkeslett.hour}:{klokkeslett.minute} - {temperatur} - {regn}mm")
 
-for forecast in weather.forecast(str):
-    data = json.loads(forecast)
-    temp = data['temperature']
-    rain = data['precipitation']
-    regn = rain['@value']
-    temperatur = temp['@value']
-    if(klokkeslett.minute == 0):
-        print(f"kl {klokkeslett.hour}:00 - {temperatur} - {regn}mm")
-    else:
-        print(f"kl {klokkeslett.hour}:{klokkeslett.minute} - {temperatur} - {regn}mm")
+        klokkeslett = klokkeslett + datetime.timedelta(minutes=30)
 
-    klokkeslett = klokkeslett + datetime.timedelta(minutes=30)
+schedule.every().day.at("03:25").do(sjekkVaer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
